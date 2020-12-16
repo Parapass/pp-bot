73fbe5e7b2a7e2e580bc37d942cbaab514c80e1f
Parapass <59424861+Parapass@users.noreply.github.com>
2020-12-16 18:29:35 -0500

    Update core.js

--- core.js
+++ core.js
@@ -7,19 +7,34 @@ const PREFIX = "-"
 client.on("ready", () => {
   console.log('Bot is running with "+client.users.size+" users, in  "+client.channels.size+" channels of "+client.guilds.size+" guilds! 👍')
 })
+// greeting command.
+bot.on('guildMemberAdd', member =>{
+
+    const channel = member.guild.channels.cache.find(channel => channel.name === "general");
+    if(!channel) return;
+
+    channel.send(`Hello ${member}!`)
+    var greetingEmbed = new Discord.MessageEmbed()
+        greetingEmbed.setColor('##ffe100')
+        greetingEmbed.setAuthor('Hello!')
+        greetingEmbed.setTitle('Welcome! Be sure to read <#775164569437077524> and <#775165374210768976>')
+        greetingEmbed.setImage(member.user.displayAvatarURL())
+        greetingEmbed.setThumbnail("https://cdn.discordapp.com/emojis/733509059168174190.gif?v=1")
+    channel.send(greetingEmbed);
+});
 //on leaving message
 client.on('guildMemberRemove', member => {
 
-    const channel = member.guild.channels.cache.find(channel => channel.name === "new-peeps");
+    const channel = member.guild.channels.cache.find(channel => channel.name === "general");
     if(!channel) return;
 
     channel.send(`**${member}** has just left the server.. Bye Bye`);
     var leavingEmbed = new Discord.MessageEmbed()
         leavingEmbed.setColor('#0099ff')
-        leavingEmbed.setAuthor('PepeLa.')
-        leavingEmbed.setTitle('Why must you leave, now we do the PepeLa!')
+        leavingEmbed.setAuthor('Cya.')
+        leavingEmbed.setTitle('Thanks for stopping by!')
         leavingEmbed.setImage(member.user.displayAvatarURL())
-        leavingEmbed.setThumbnail('https://cdn.discordapp.com/emojis/727552232446230578.png?v=1')
+        leavingEmbed.setThumbnail('https://cdn.discordapp.com/emojis/785455324768632832.gif?v=1')
     channel.send(leavingEmbed);
 });
 client.login(process.env.BOT_TOKEN)
